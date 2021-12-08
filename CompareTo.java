3bb86cece4f20cc7193a648beb9c9ea24e973d2a
manjugoutham <gouthamy219@gmail.com>
2021-12-08 10:31:09 +0530

    Days3 LFP problems

---
+++ CompareTo.java
@@ -0,0 +1,17 @@
+class CompareTo
+{
+public static void main(String args[])
+{
+String str1 = "demo";
+
+String str2 = "";
+
+String str3 = "lines";
+
+System.out.println(str1.compareTo(str2));
+
+System.out.println(str2.compareTo(str3));
+
+}
+}
+
